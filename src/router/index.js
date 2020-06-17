84a372aeb4469e7d305365dd9f131020382edf25
Charles Obeng <charlesobeng000@gmail.com>
2020-06-17 17:55:07 +0000

    folder structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,28 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+
+import MainComponent from "../containers/Main";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    redirect: "/dashboard",
+    name: "Main",
+    component: MainComponent,
+    chiildren: [
+      {
+        path: "/dashboard",
+        name: "dashboard",
+        component: Dashboard
+      }
+    ]
   }
 ];
 
 const router = new VueRouter({
+  base: process.env.BASE_URL,
   routes
 });
 

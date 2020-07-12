fb6bc6b699b11eec85ce12373705f7d9057406b8
Charles Obeng <charlesobeng000@gmail.com>
2020-07-12 07:50:28 +0000

    config

--- src/config.js
+++ src/config.js
@@ -6,14 +6,14 @@ function getApiUrl() {
     return 'https://api.fppghana.com/api/v1/'
   }
 
-  return 'http://127.0.0.1:5000/api/v1/'
+  return 'https://api.fppghana.com/api/v1/'
 }
 
 const API_URL = getApiUrl();
 
 export default {
   LOGIN_URL: API_URL + 'auth/login',
-  REGISTER_URL: API_URL + 'aurh/register',
+  REGISTER_URL: API_URL + 'auth/register',
   LOGGED_IN_USER_URL: API_URL + 'auth/current-user',
   LOGOUT_USER_URL: API_URL + 'auth/logout',
   FARMERS_URL: API_URL + 'farmers',

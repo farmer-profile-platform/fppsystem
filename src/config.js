062a13c1c5850ceb1d11307b8eeffff02ef413af
Charles Obeng <charlesobeng000@gmail.com>
2020-10-19 13:37:34 +0000

    config

--- src/config.js
+++ src/config.js
@@ -3,9 +3,9 @@ function getApiUrl() {
   if (hn === 'localhost') {
     return 'http://127.0.0.1:5000/api/v1/'
   } else if (hn == 'fppghana.com') {
-    return 'https://api.fppghana.com/api/v1/'
+    return 'https://api-farmer-pro.herokuapp.com/api/v1/'
   } else {
-    return 'https://api.fppghana.com/api/v1/'
+    return 'https://api-farmer-pro.herokuapp.com/api/v1/'
   }
 }
 

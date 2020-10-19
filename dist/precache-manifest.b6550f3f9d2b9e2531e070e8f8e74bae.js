de80111a42be0a46757d05046b621673c5fa95d2
Charles Obeng <charlesobeng000@gmail.com>
2020-10-19 13:52:40 +0000

    dist build

---
+++ dist/precache-manifest.b6550f3f9d2b9e2531e070e8f8e74bae.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e44344139fe60a4949a",
+    "url": "/css/app.3f12ed6d.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "1dece85edc1bcf48679e43ded9cbc8e9",
+    "url": "/img/farmer.1dece85e.jpg"
+  },
+  {
+    "revision": "bc6e08809256c5cd095cf702f76eccb1",
+    "url": "/img/favicon.png"
+  },
+  {
+    "revision": "aeab7e15d87722c5628919712e796135",
+    "url": "/img/fpplogo.aeab7e15.png"
+  },
+  {
+    "revision": "3700c75cf7fc446290bf3e92b1078610",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e44344139fe60a4949a",
+    "url": "/js/app.29e3bb20.js"
+  },
+  {
+    "revision": "7192462ad56ab8dca5a8",
+    "url": "/js/chunk-vendors.1f550223.js"
+  },
+  {
+    "revision": "76e52371e36d3d24875c2415c340088f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);

a3e66bc216f70e45587c23924cb3138f6649de65
Charles Obeng <charlesobeng000@gmail.com>
2020-10-30 23:47:35 +0000

    version update

--- src/containers/Main.vue
+++ src/containers/Main.vue
@@ -14,7 +14,7 @@
         </el-main>
         <footer class="flex_justify_between">
           <p>Farmer Pro Platform © 2020. All right reserved.</p>
-          <p>v-0.5.9</p>
+          <p>v-0.8.2</p>
         </footer>
       </el-container>
     </el-container>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -61,7 +61,7 @@
         <br />
         <p style="text-align:center;">
           Only authorized company staff will have access. Log In details is
-          therefore 'Requested'. (v-0.5.9)
+          therefore 'Requested'. (v-0.8.2)
         </p>
       </el-col>
     </el-row>

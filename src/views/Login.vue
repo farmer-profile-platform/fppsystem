9815d36ee48e98b024b20119adb5608475c1fa54
Charles Obeng <charlesobeng000@gmail.com>
2020-12-03 02:52:10 +0000

    version

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -61,7 +61,7 @@
         <br />
         <p style="text-align:center;">
           Only authorized company staff will have access. Log In details is
-          therefore 'Requested'. (v-1.0.9)
+          therefore 'Requested'. (v-1.5.0)
         </p>
       </el-col>
     </el-row>

e5e912604b1f6c54ca2e8b2578b1df03f225000d
charles2k16 <charlesprogram@gmail.com>
2021-08-25 03:51:37 +0000

    new deploy to main

---
+++ dist/precache-manifest.87fca81b41111c4d5fc7c8cb6b02e0c3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc7478abcf84b668352",
+    "url": "/css/app.515d517c.css"
+  },
+  {
+    "revision": "de6a98cab0d853e0ef55",
+    "url": "/css/chunk-c9ef0f2e.37d9cc6b.css"
+  },
+  {
+    "revision": "7802d8b27fcb19893ce6b38c0789268e",
+    "url": "/fonts/Raleway-Bold.ttf"
+  },
+  {
+    "revision": "f73026bcd64e5a5265ab616e5083cd48",
+    "url": "/fonts/Raleway-Italic.ttf"
+  },
+  {
+    "revision": "a55ff2cd6e2cffc65817240e14da6813",
+    "url": "/fonts/Raleway-MediumItalic.ttf"
+  },
+  {
+    "revision": "75b4247fdd3b97d0e3b8e07b115673c2",
+    "url": "/fonts/Raleway-Regular.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "1dece85edc1bcf48679e43ded9cbc8e9",
+    "url": "/img/farmer.1dece85e.jpg"
+  },
+  {
+    "revision": "aeab7e15d87722c5628919712e796135",
+    "url": "/img/fpplogo.aeab7e15.png"
+  },
+  {
+    "revision": "71541182c6765ec4eeaf73060e94da9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dc7478abcf84b668352",
+    "url": "/js/app.d993756d.js"
+  },
+  {
+    "revision": "de6a98cab0d853e0ef55",
+    "url": "/js/chunk-c9ef0f2e.5db85664.js"
+  },
+  {
+    "revision": "49b884b3138c539f7d38",
+    "url": "/js/chunk-vendors.82ba39e7.js"
+  },
+  {
+    "revision": "76e52371e36d3d24875c2415c340088f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);

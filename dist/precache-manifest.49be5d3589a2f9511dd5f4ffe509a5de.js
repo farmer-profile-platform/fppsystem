c69d54572acff0f064aa94ce655c87e4eeb9d1eb
Charles Kwabena <charlesprogram@gmail.com>
2023-02-22 03:27:06 +0000

    remove dist from ignore

---
+++ dist/precache-manifest.49be5d3589a2f9511dd5f4ffe509a5de.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a94f719fa572f6fd45b8",
+    "url": "/css/app.64433702.css"
+  },
+  {
+    "revision": "acac654bc363340d8556",
+    "url": "/css/chunk-4468c93c.37d9cc6b.css"
+  },
+  {
+    "revision": "7802d8b27fcb19893ce6b38c0789268e",
+    "url": "/fonts/Raleway-Bold.ttf"
+  },
+  {
+    "revision": "f73026bcd64e5a5265ab616e5083cd48",
+    "url": "/fonts/Raleway-Italic.ttf"
+  },
+  {
+    "revision": "a55ff2cd6e2cffc65817240e14da6813",
+    "url": "/fonts/Raleway-MediumItalic.ttf"
+  },
+  {
+    "revision": "75b4247fdd3b97d0e3b8e07b115673c2",
+    "url": "/fonts/Raleway-Regular.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "1dece85edc1bcf48679e43ded9cbc8e9",
+    "url": "/img/farmer.1dece85e.jpg"
+  },
+  {
+    "revision": "aeab7e15d87722c5628919712e796135",
+    "url": "/img/fpplogo.aeab7e15.png"
+  },
+  {
+    "revision": "2b23e93185eb219860d552cd58d32bb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a94f719fa572f6fd45b8",
+    "url": "/js/app.e07a2b4b.js"
+  },
+  {
+    "revision": "acac654bc363340d8556",
+    "url": "/js/chunk-4468c93c.06e3d86c.js"
+  },
+  {
+    "revision": "b8045eec20689aef4301",
+    "url": "/js/chunk-vendors.5f53ca42.js"
+  },
+  {
+    "revision": "76e52371e36d3d24875c2415c340088f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
